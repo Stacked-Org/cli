ccbd5854fd835430649ade4e09b76b8bfb8555ff
Fernando Ferrara <ferrarafer@gmail.com>
2023-05-23 10:30:54 -0300

    feat: add watch argument to generate command (#20)
    * Add watch flag to generate command
    
    * Update build runner command
    
    - Updated build_runner command to run with Dart instead of Flutter which
      was deprecated on version 3.10.
    - Add delete-conflicting-outputs flag to allow disable of it.
    
    * Fix format
    
    ---------
    
    Co-authored-by: Dane Mackier <dane.mackier@outlook.com>

--- lib/src/templates/compiled_templates.dart
+++ lib/src/templates/compiled_templates.dart
@@ -1925,7 +1925,7 @@ const String kAppWebTemplateSettingsJsonStkContent = '''
 {
     "explorer.fileNesting.enabled": true,
     "explorer.fileNesting.patterns": {
-        "*.dart": "\$\{capture}.mobile.dart, \$\{capture}.tablet.dart, \$\{capture}.desktop.dart, \$\{capture}.form.dart, \$\{capture}.g.dart, \$\{capture}.freezed.dart, \$\{capture}.logger.dart, \$\{capture}.locator.dart, \$\{capture}.router.dart, \$\{capture}.dialogs.dart, \$\{capture}.bottomsheets.dart"
+        "*.dart": "\${capture}.mobile.dart, \${capture}.tablet.dart, \${capture}.desktop.dart, \${capture}.form.dart, \${capture}.g.dart, \${capture}.freezed.dart, \${capture}.logger.dart, \${capture}.locator.dart, \${capture}.router.dart, \${capture}.dialogs.dart, \${capture}.bottomsheets.dart"
     }
 }
 
@@ -3138,7 +3138,7 @@ const String kAppMobileTemplateSettingsJsonStkContent = '''
 {
     "explorer.fileNesting.enabled": true,
     "explorer.fileNesting.patterns": {
-        "*.dart": "\$\{capture}.mobile.dart, \$\{capture}.tablet.dart, \$\{capture}.desktop.dart, \$\{capture}.form.dart, \$\{capture}.g.dart, \$\{capture}.freezed.dart, \$\{capture}.logger.dart, \$\{capture}.locator.dart, \$\{capture}.router.dart, \$\{capture}.dialogs.dart, \$\{capture}.bottomsheets.dart"
+        "*.dart": "\${capture}.mobile.dart, \${capture}.tablet.dart, \${capture}.desktop.dart, \${capture}.form.dart, \${capture}.g.dart, \${capture}.freezed.dart, \${capture}.logger.dart, \${capture}.locator.dart, \${capture}.router.dart, \${capture}.dialogs.dart, \${capture}.bottomsheets.dart"
     }
 }
 
