f08e730324e00351857649672afb29f4a416cb46
Pebkac03 <pilutalej@hotmail.se>
2024-12-12 18:22:54 +0100

    fix: force unix path separators in compiled_templates.dart

--- lib/src/templates/compiled_constants.dart
+++ lib/src/templates/compiled_constants.dart
@@ -1,26 +1,26 @@
-/// NOTE: This is generated code from the compileTemplates command. Do not
+/// NOTE: This is generated code from the compileTemplates command. Do not 
 ///       modify by hand.
 ///       This file should be checked into source control.
 
 Map<String, List<String>> kCompiledTemplateTypes = {
   'app': [
-    'web',
     'mobile',
+    'web',
   ],
-  'widget': [
+  'bottom_sheet': [
     'empty',
   ],
   'dialog': [
+    'empty',
+  ],
+  'service': [
     'empty',
   ],
   'view': [
     'empty',
     'web',
   ],
-  'service': [
-    'empty',
-  ],
-  'bottom_sheet': [
+  'widget': [
     'empty',
   ],
 };
